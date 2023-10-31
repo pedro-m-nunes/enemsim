62d7a105dd01de5af393c1060d16328b107b0be8
Pedro Nunes <pedro.familianunes@gmail.com>
2023-10-31 09:49:08 -0300

    Trocando localhost no url.js, porque preisa ser o IP da máquina que roda a API.

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const requestBaseUrl = 'http://localhost:8080/api/';
-// export const requestBaseUrl = 'http://172.16.8.139:8080/api/';
+// export const requestBaseUrl = 'http://localhost:8080/api/';
+export const requestBaseUrl = 'http://172.16.8.139:8080/api/';
